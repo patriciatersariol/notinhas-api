aae16f6dfe909da698f8b1a7d3b1ac6bf5953ffb
patricia <patriciatersariol@gmail.com>
2021-11-07 19:44:47 -0300

    create functions to routes get, post and get by id on Notebook

--- src/api/routes/notebook.routes.ts
+++ src/api/routes/notebook.routes.ts
@@ -1,9 +1,48 @@
 import { Request, Response, Router } from "express"
+import { getRepository } from "typeorm"
+import { Note } from "../../entities/Note"
+import { Notebook } from "../../entities/Notebook"
 const router = Router()
 
-router.get('/notebook', (req, res) => res.json('First route is working!'))
-router.post('/notebook', (req, res) => res.json(req.body))
-router.get('notebook/:id', (req, res) => res.json('id'))
+router.get('/notebook', async (req, res) => {
+    const notebooks = await getRepository(Notebook).find()
+    return res.json(notebooks)
+})
+
+router.post('/notebook', async (req, res) => {
+
+    const teste = req.body
+
+    const newNotebookReq = {
+        name: req.body.name,
+        description: req.body.description ,
+        category: req.body.category
+    }
+    
+    const notebookRepo = getRepository(Notebook)
+    const notebook = notebookRepo.create(newNotebookReq)
+
+    try {
+        const result = await notebookRepo.save(notebook)
+        return res.json(result)
+    } catch (error) {
+        return res.json(error)
+    }
+})
+
+
+router.get('/notebook/:id/notes', async (req, res) => {
+    const notebookId = req.params.id
+    const notesRepo = getRepository(Note)
+    
+    try {
+        const result = await notesRepo.find({ where: {notebook: notebookId}})
+        return res.json(result)
+    }catch(error) {
+        return res.json(error)
+    }
+    
+})
 router.delete('notebook/:id', (req, res) => res.json('id'))
 router.patch('notebook/:id', (req, res) => res.json('id'))
 
