f23a2ccc0670c4c9ce8966f9556b21183eb29226
patricia <patriciatersariol@gmail.com>
2021-11-07 19:46:30 -0300

    create function to post route on Note

--- src/api/routes/note.routes.ts
+++ src/api/routes/note.routes.ts
@@ -1,8 +1,28 @@
 import { Request, Response, Router } from "express"
+import { getRepository } from "typeorm"
+import { Note } from "../../entities/Note"
 const router = Router()
 
 router.get('/note', (req, res) => res.json('Primeira rota das notas!'))
-router.post('/note', (req, res) => res.json(req.body))
+
+router.post('/note', async (req, res) => {
+    const newNoteReq = {
+        title: req.body.title,
+        subtitle: req.body.subtitle,
+        text: req.body.text,
+        notebookId: req.body.notebook
+    }
+    const noteRepo = getRepository(Note)
+    const newNote = noteRepo.create(newNoteReq)
+    try {
+        const result = await noteRepo.save(newNote)
+        return res.json(result)
+    }catch(error) {
+        return res.json(error)
+    }
+    
+    
+})
 router.get('note/:id', (req, res) => res.json('id'))
 router.delete('note/:id', (req, res) => res.json('id'))
 router.patch('note/:id', (req, res) => res.json('id'))
