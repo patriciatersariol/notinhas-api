ebd040fe5f70acc5b61480dfb4caf5b4a36c1ab5
patricia <patriciatersariol@gmail.com>
2021-11-28 16:33:22 -0300

    removes connection creation from index

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,18 @@
 import 'reflect-metadata'
 import express from 'express'
 import cors from 'cors'
-import { createConnection } from 'typeorm'
 
 import notebookRoutes from './api/routes/notebook.routes'
 import noteRoutes from './api/routes/note.routes'
 
 const app = express()
-createConnection()
-
+    
 //middlewares
-
 app.use(cors())
 app.use(express.json())
-
-//routes
+    
+    //routes
 app.use(notebookRoutes)
 app.use(noteRoutes)
 
-
-app.listen(3000, () => console.log('Server running on port 3000'))
-
+module.exports = app
