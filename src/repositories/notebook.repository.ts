6286a8e6af334e100e963833ad904460495d43dc
patricia <patriciatersariol@gmail.com>
2021-11-28 16:24:54 -0300

    includes a DTO interface for notebook

--- src/repositories/notebook.repository.ts
+++ src/repositories/notebook.repository.ts
@@ -1,10 +1,19 @@
 import { getRepository } from "typeorm";
 import { Notebook } from "../entities/Notebook";
-import { INotebookDB } from "../interfaces/notebook.interface";
+import { INotebookDTO, INotebookInput } from "../interfaces/notebook.interface";
 
-export const saveNewNotebook = async (newNotebook: Object): Promise<INotebookDB> => {
+export const saveNewNotebook = async (newNotebook: INotebookInput): Promise<INotebookDTO> => {
     const notebookRepo = getRepository(Notebook)
     const notebook = notebookRepo.create(newNotebook)
-    const result: INotebookDB = await notebookRepo.save(notebook)
-    return  result
+    const result = await notebookRepo.save(notebook)
+
+    const createdNotebook: INotebookDTO = {
+        name: result.name,
+        description: result.description,
+        category: result.category,
+        createdAt: result.createdAt,
+        updatedAt: result.updatedAt,
+    }  
+    
+    return createdNotebook
 }
