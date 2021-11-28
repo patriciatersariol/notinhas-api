ff6f6e46d981c56e2f250e43ebffdbf5556e0afe
patricia <patriciatersariol@gmail.com>
2021-11-28 16:28:18 -0300

    modifies types of params and return of notebook controller

--- src/controllers/notebook.controller.ts
+++ src/controllers/notebook.controller.ts
@@ -1,20 +1,20 @@
 import { Request, Response } from "express"
 import { Notebook } from "../entities/Notebook"
-import { IBaseNotebook } from "../interfaces/notebook.interface"
+import { INotebookDTO, INotebookInput } from "../interfaces/notebook.interface"
 import {save} from "../services/notebook.service"
 
 export const createNotebook = async (request: Request, response: Response): Promise<Response> => {
-    const notebookData: IBaseNotebook = request.body
+    const notebookData: INotebookInput = request.body
     console.log(notebookData)
 
-    const newNotebook: IBaseNotebook = {
+    const newNotebook: INotebookInput = {
         name: notebookData.name,
         description: notebookData.description,
         category: notebookData.category
     }
 
     try {
-        const result = await save(newNotebook)
+        const result: INotebookDTO = await save(newNotebook)
         return response.json(result)
     } catch(error) {
         return response.json(error)
