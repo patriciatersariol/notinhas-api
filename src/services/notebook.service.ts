61797b72d303a4a672a9d8edc3c249563e00976b
patricia <patriciatersariol@gmail.com>
2021-11-28 16:23:06 -0300

    modify params and return types on notebook.service

--- src/services/notebook.service.ts
+++ src/services/notebook.service.ts
@@ -1,6 +1,7 @@
 import { Notebook } from "../entities/Notebook";
+import { INotebookDTO, INotebookInput } from "../interfaces/notebook.interface";
 import { saveNewNotebook } from "../repositories/notebook.repository";
 
-export const save = async (newNotebook: Object): Promise<Notebook> => {
+export const save = async (newNotebook: INotebookInput): Promise<INotebookDTO> => {
     return await saveNewNotebook(newNotebook)
 }
