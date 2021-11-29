23232b7eda1bce4e313230e92c5d483ba592d561
patricia <patriciatersariol@gmail.com>
2021-11-29 00:19:03 -0300

    moves routes tests to the correct folder

--- src/api/routes/__tests__/notebook.routes.ts
+++ src/api/routes/__tests__/notebook.routes.ts
@@ -1,14 +1,15 @@
 import request from 'supertest'
-import createDbConnection from '../../dbConnection'
-const app = require('../../index')
+import createDbConnection from '../../../dbConnection'
+const app = require('../../../index')
 
 beforeAll(async () => {
     await createDbConnection()
 })
 
-test('should return status code 200', async () => {
-    const result = await request(app).get("/health")
-    expect(result.statusCode).toEqual(203)
+test('should return status code 200 and OK response', async () => {
+    const response = await request(app).get("/health")
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toBe("OK")
 })
 
 test('should call the createNotebook function', async () => {
